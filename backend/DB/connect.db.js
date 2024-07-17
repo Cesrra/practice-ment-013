84dd28745f63b3ef435fe6f158961c2075a7d7bd
tamsaleh <t.saleh.gc@gmail.com>
2024-07-17 17:58:54 -0300

    ADD: added a cloud db instance for deployment purposes

--- backend/DB/connect.db.js
+++ backend/DB/connect.db.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  const { CONN } = process.env;
+  const { CONN, cloud_db_conn } = process.env;
 
   try {
-    await mongoose.connect(CONN);
+    await mongoose.connect(cloud_db_conn);
     console.log("Connected to DB");
   } catch (error) {
     console.error(error.message)
