319407d89ebd42457466c5f3b2460d581127b6df
Pedro Fernandez <pefernandez1408@gmail.com>
2024-07-17 14:21:43 -0400

    ADD: creation of the first stories

---
+++ frontend/src/stories/signup/SignUp.stories.ts
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+//import { fn } from '@storybook/test';
+import SignUp from '../../components/SignUp';
+
+const meta = {
+    title: 'Example/SignUp',
+    component: SignUp,
+    parameters: {
+        layout: 'centered',
+    }
+} satisfies Meta<typeof SignUp>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+    args: {
+      primary: true
+    },
+  };
