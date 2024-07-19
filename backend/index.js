b69c7615ccf6b2ebaab923c820d0f27c78bf27dd
tamsaleh <t.saleh.gc@gmail.com>
2024-07-19 16:21:38 -0300

    FIX: added one origin for CORS

--- backend/index.js
+++ backend/index.js
@@ -2,26 +2,23 @@ import express from 'express'
 import connectDB from './DB/connect.db.js';
 import middlewares from './middlewares/app.middleware.js';
 
+const app = express();
+middlewares.forEach(mw => { app.use(mw) });
+
+const { PROD_PORT, DEV_PORT, NODE_ENV } = process.env;
+const PORT = NODE_ENV === 'production' ? PROD_PORT : DEV_PORT;
+
 const connectApp = async () => {
 
   try {
     await connectDB();
     app.listen(PORT, () => {
       console.log(`Listening on port ${PORT}`);
-    })
+    });
   } catch (error) {
     console.log(error.message);
-  }
-}
-
-
-const app = express();
-
-
-middlewares.forEach(mw => { app.use(mw) });
-
-const { PROD_PORT, DEV_PORT, NODE_ENV } = process.env;
-const PORT = NODE_ENV === 'production' ? PROD_PORT : DEV_PORT;
+  };
+};
 
 connectApp();
 
