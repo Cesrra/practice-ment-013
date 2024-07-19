8b8cc078d47b7c830e7812f034f3135990a5d230
Marco Perez Donoso <marcopdonoso@gmail.com>
2024-07-19 11:35:04 -0400

    Revert changes in backend/

--- backend/DB/connect.db.js
+++ backend/DB/connect.db.js
@@ -1,15 +1,15 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const connectDB = async () => {
-	const { CONN, cloud_db_conn, NODE_ENV } = process.env
-	const conString = NODE_ENV === 'production' ? cloud_db_conn : CONN
+  const { CONN, cloud_db_conn, NODE_ENV } = process.env;
+  const conString = NODE_ENV === 'production' ? cloud_db_conn : CONN;
 
-	try {
-		await mongoose.connect(conString)
-		console.log('Connected to DB')
-	} catch (error) {
-		console.error(error.message)
-	}
+  try {
+    await mongoose.connect(conString);
+    console.log("Connected to DB");
+  } catch (error) {
+    console.error(error.message)
+  }
 }
 
-export default connectDB
+export default connectDB;
--- backend/utils/auth/auth.utils.js
+++ backend/utils/auth/auth.utils.js
@@ -1,36 +1,48 @@
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
-const { SALT_ROUNDS, JWT_SECRET, NODE_ENV } = process.env
+const { SALT_ROUNDS, JWT_SECRET, NODE_ENV } = process.env;
 
 const getHashedPassword = async (password) => {
-	try {
-		const salt = await bcrypt.genSalt(+SALT_ROUNDS)
-		return await bcrypt.hash(password, salt)
-	} catch (error) {
-		throw new Error('Error during hashing password: ' + error.message)
-	}
+  try {
+    const salt = await bcrypt.genSalt(+SALT_ROUNDS);
+    return await bcrypt.hash(password, salt);
+  } catch (error) {
+    throw new Error("Error during hashing password: " + error.message)
+  }
 }
 
 const generateTokenizedCookie = (userId, res) => {
-	try {
-		const token = jwt.sign({ userId }, JWT_SECRET, {
-			expiresIn: '15d',
-		})
 
-		res.cookie('jwt', token, {
-			maxAge: 15 * 24 * 60 * 60 * 1000,
-			secure: NODE_ENV === 'production',
-			httpOnly: true,
-			sameSite: NODE_ENV === 'production' ? 'none' : 'lax',
-		})
-	} catch (error) {
-		return res.status(500).json({ token_error: error.message })
-	}
+  try {
+    const token = jwt.sign({ userId }, JWT_SECRET, {
+      expiresIn: '15d'
+    });
+
+    res.cookie('jwt', token, {
+      maxAge: 15 * 24 * 60 * 60 * 1000,
+      secure: NODE_ENV === 'production',
+      httpOnly: true,
+      sameSite: 'strict'
+    })
+  } catch (error) {
+    return res.status(500).json({ 'token_error': error.message })
+  }
+
 }
 
 const isValidPassword = async (plainPass, hashedPass) => {
-	return await bcrypt.compare(plainPass, hashedPass)
+  return await bcrypt.compare(plainPass, hashedPass);
 }
 
-export { generateTokenizedCookie, getHashedPassword, isValidPassword }
+
+
+
+
+export { getHashedPassword, generateTokenizedCookie, isValidPassword }
+
+
+
+
+
+
