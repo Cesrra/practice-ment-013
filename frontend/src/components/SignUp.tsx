ca8d529cad9f0731c0e67481e8d5ac28a55a4055
Pedro Fernandez <pefernandez1408@gmail.com>
2024-07-18 21:01:13 -0400

    ADD: Sign Up form

--- frontend/src/components/SignUp.tsx
+++ frontend/src/components/SignUp.tsx
@@ -1,33 +1,76 @@
+import { useState } from "react"
+import InputField from "./login/InputField"
+import Button from "./Button"
+import LinkText from "./login/LinkText"
 
 export default function SignUp() {
+
+	const [email, setEmail] = useState('')
+	const [user, setUser] = useState('')
+	const [password, setPassword] = useState('')
+	const [repeatPassword, setReapetPassword] = useState('')
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+	}
+
+	const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		e.preventDefault()
+		setEmail(e.target.value)
+	}
+
+	const handleUserChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		e.preventDefault()
+		setUser(e.target.value)
+	}
+
+	const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		e.preventDefault()
+		setPassword(e.target.value)
+	}
+
     return(
         <section>
-            <h1>Create Account!</h1>
-			<form>
-                <label htmlFor="">Email</label>
-				<input
+			<form 
+				className="flex flex-col gap-4"
+				onSubmit={handleSubmit}
+				>
+				<InputField
+					label="Email"
 					type="email"
-					placeholder="your email"
+					value={email}
+					className="w-[416px]"
+					onChange={handleEmailChange}
 				/>
-                <label htmlFor="">Username</label>
-				<input
+				<InputField
+					label="Username"
 					type="text"
-					placeholder="your username"
+					value={user}
+					className="w-[416px]"
+					onChange={handleUserChange}
 				/>
-                <label htmlFor="">Password</label>
-                <input
-					type="password"
-					placeholder="password"
+				<InputField
+					label="Password"
+					type="text"
+					value={password}
+					className="w-[416px]"
+					onChange={handlePasswordChange}
 				/>
-                <label htmlFor="">Reapeat yor password</label>
-                <input
-					type="password"
+				<InputField
+					label="Repeat your password"
+					type="text"
+					value={repeatPassword}
+					className="w-[416px]"
 				/>
-				<button type="submit">Sign Up</button>
+				<Button
+					type="submit"
+					className="h-11"
+				>
+					Sign Up
+				</Button>
 			</form>
-			<p>
-			<a>Do you already have an account?</a>
-			</p>
+			<LinkText className="mt-1">Do you already have an account?</LinkText>
+
         </section>
     )
 }
